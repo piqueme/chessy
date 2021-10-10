e73345b7a2c96aeb99e3c497a5d7d41ee3cd1518
Sumit Gogia <sumit.y.gogia@gmail.com>
2021-10-09 19:59:55 -0700

    get Jest for web working as well

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,10 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-react',
     '@babel/preset-typescript'
+  ],
+  plugins: [
+    "babel-plugin-transform-import-meta"
   ]
 };
--- jest.config.base.js
+++ jest.config.base.js
@@ -2,12 +2,12 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     "**/*.{ts,tsx}",
-    "!**/*.test.{ts.tsx}"
+    "!**/*.test.{ts,tsx}"
   ],
   coverageDirectory: "<rootDir>/reports",
   injectGlobals: true,
   verbose: true,
   // block files from being imported...probably not other tests??
   testEnvironment: "node",
-  testMatch: ["**/__tests__/*.test.ts"],
+  testMatch: ["**/__tests__/*.test.{ts,tsx}"],
 }
